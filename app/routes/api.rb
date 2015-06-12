94633391c31a5fda44c736943578d5b926c2544b
Matt Cowski <Matt.Cowski@gmail.com>
2015-06-12 11:26:37 -0500

    fixed /api/locations to allow son

--- app/routes/api.rb
+++ app/routes/api.rb
@@ -2,7 +2,8 @@ module Vitos
   module Routes
     class API < Base
       get '/api/locations' do
-        json Store.where(isActive: true)
+        @locations = Store.where(isActive: true)
+        json @locations
       end
       get '/api/me' do
         json current_user
